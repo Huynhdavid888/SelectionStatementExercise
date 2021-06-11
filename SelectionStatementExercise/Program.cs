54bbe64e3c87654ca093a3c4a48bbfa78504acbc
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-11 14:24:59 -0700

    initial commit

---
+++ SelectionStatementExercise/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SelectionStatementExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            var r = new Random();
+            var favNumber = r.Next(1, 50);
+
+            Console.WriteLine("Try to guess my favorite number");
+            var userinput = int.Parse(Console.ReadLine());
+
+            if (favNumber == 25)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            else if (favNumber < 20)
+            {
+                Console.WriteLine("So close!");
+            }
+
+
+            else if (favNumber < 10)
+            {
+                Console.WriteLine("Not even close");
+            }
+
+            else if (favNumber > 25)
+            {
+                Console.WriteLine("you're right there!");
+
+            }
+
+            else if (favNumber > 30)
+            {
+                Console.WriteLine("You're getting farther away");
+
+            }
+
+            else 
+            {
+                Console.WriteLine("Just give up");
+            }
+
+            #endregion
+        }
+
+    }
+}
